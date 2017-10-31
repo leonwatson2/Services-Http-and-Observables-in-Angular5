869e3a8482673e23183e52090e3f08d9892cbbe1
vlw0052 <leon@vlw2.com>
2017-10-31 14:30:25 -0500

    Observables1

--- src/app/title.service.ts
+++ src/app/title.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/Rx'
 
 @Injectable()
 export class TitleService {
   title:string = "Service"
-  constructor() { 
-    setTimeout(()=>{
-      this.title = "New Service"
-    }, 3000)
+  constructor() {
+    const number = Observable.interval(1000)    
+    number.subscribe((number)=>{
+      this.title = `${number}`
+    })
   }
 
 }
