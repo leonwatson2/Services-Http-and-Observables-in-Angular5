c1133e163ccfa1784783937c40e2b1b4d8efff97
vlw0052 <leon@vlw2.com>
2017-10-31 13:30:46 -0500

    Adding Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { TitleService } from './title.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ TitleService ]
 })
 export class AppComponent {
-  title = 'app';
+  title:string;
+
+  constructor(private titleService:TitleService){}
+
+  ngOnInit(){
+    this.title = this.titleService.title  
+  }
 }
---
+++ src/app/title.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TitleService {
+  title:string = "Service"
+  constructor() { }
+
+}
